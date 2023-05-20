76f3b9602f113ee6726c07ce2d28d18e0378a459
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-05-20 10:07:57 +0200

    docker

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,7 @@ const config = {
     /// Set this to false if you want production builds to abort if there's lint errors
     ignoreDuringBuilds: process.env.VERCEL_ENV === 'production',
   },
+  output: 'standalone',
 }
 
 export default config
