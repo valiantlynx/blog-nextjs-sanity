af7b5487de7051b849b057c428d970cfec7511f5
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-06-06 01:58:03 +0200

    Update _document.tsx
    fix again

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,15 +6,19 @@ export default function Document() {
     <Html lang="en">
     
       <Head>
-        <Script id="clarity-toseeshit" strategy="afterInteractive">
-          {`
-        (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", '${process.env.CLARITY_KEY}');
-        `}
-        </Script>
+     
+<Script
+        id="clarity-toseestuff"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", '${process.env.CLARITY_KEY}');`,
+        }}
+      />
         <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7827487944601896" strategy="afterInteractive" />
         <Script
           strategy="afterInteractive"
