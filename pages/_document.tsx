e3f995e7ab499af06c8f4de78b4a7bbe468a5a7f
valiantlynx <86688436+valiantlynx@users.noreply.github.com>
2023-06-06 01:52:34 +0200

    Update _document.tsx
    fix clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,13 +6,13 @@ export default function Document() {
     <Html lang="en">
     
       <Head>
-        <Script id="clarity" strategy="afterInteractive">
+        <Script id="clarity-toseeshit" strategy="afterInteractive">
           {`
         (function(c,l,a,r,i,t,y){
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-      })(window, document, "clarity", "script", "ftpnw924g7");
+      })(window, document, "clarity", "script", '${process.env.CLARITY_KEY}');
         `}
         </Script>
         <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7827487944601896" strategy="afterInteractive" />
